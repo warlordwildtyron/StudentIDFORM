415102754b20eda43044a36bbe2ce3b8b77e7f59
warlordwildtyron <81098671+warlordwildtyron@users.noreply.github.com>
2022-10-17 19:21:16 +0800

    Add files via upload

---
+++ STUDENTIDFORM.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name= "viewpoint" content="width=device-width, initial-scale=1">
+<title> Student ID Form </title>
+</head>
+<body>
+<form action="" method="GET">
+First Name: <input type = "text" name = "fname" size= "20" maxlength = "10"><br>
+Middle Initial: <input type = "text" name = "minitial" size= "20" maxlength = "1"><br>
+Last Nam:e <input type = "text" name = "lname" size= "20" maxlength = "20"><br>
+SCHOOL ID NUMBER: <input type = "text" name = "idnumber" size = "20" maxlength = "9"><br>
+<br>
+<textarea rows = "4" cols "50">
+ADDRESS
+</textarea>
+<br>
+YEAR LEVEL
+<br>
+<select name = "YearLevel">
+<option>1st Year</option>
+<option>2nd Year</option>
+<option selected = "selected">3rd Year</option>
+<br>
+</select><br>
+<h3> COLLEGE DEPARTMENT </h3>
+<input type = "radio" name = "gg" value = "CITCS" checked = "checked"> CITCS <br>
+<input type = "radio" name = "gg" value = "COA"> COA <br>
+<input type = "radio" name = "gg" value = "CEA"> CEA <br>
+<input type = "radio" name = "gg" value = "CTE"> CTE <br>
+<input type = "radio" name = "gg" value = "CCJE"> CCJE <br>
+<br>
+
+<input type = "submit" name = "Submit">
+
+</body>
+</html>
